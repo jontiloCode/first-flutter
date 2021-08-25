7fc25222684258687d8814329f8efd5240abadb8
teddy19supangkat <teddy@munalively.com>
2021-08-25 17:16:33 +0700

    update wording

--- first_flutter/lib/main.dart
+++ first_flutter/lib/main.dart
@@ -72,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
                onPressed: () {
 
                },
-               child: Text('Latihan'),
+               child: Text('Latihan Pertama'),
              ),)
 
         ]
